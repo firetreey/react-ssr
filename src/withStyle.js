1867037fd368ac3eb5cedfa643d8a9755936008b
firetreey <firetreey@gmail.com>
2019-12-30 16:29:51 +0800

    [modify feature] use hoist-non-react-statics in withSrtle.js

--- src/withStyle.js
+++ src/withStyle.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import hoistNonReactStatic from 'hoist-non-react-statics'
 
 function withStyle(Component, styles) {
-    return function(props) {
+
+    const NewCom = (props) => {
         if (props.staticContext) {
             props.staticContext.css.push(styles._getCss())
         }
         return <Component {...props}></Component>
     }
+    hoistNonReactStatic(NewCom, Component)
+    return NewCom
 }
 
 export default withStyle
